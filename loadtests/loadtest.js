225ad8293e9d13f6343e4e6c7196d71756c97dff
Nils Schmidt <n.schmidt@micromata.de>
2024-04-12 00:22:15 +0200

    Add simple load test with K6

---
+++ loadtests/loadtest.js
@@ -0,0 +1,70 @@
+import http from "k6/http";
+import {check, sleep} from "k6";
+
+const baseUrl = "http://localhost:8080/api/v1";
+const params = {
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+};
+
+export let options = {
+    stages: [
+        {duration: "10s", target: 1000},
+        {duration: "50s", target: 1000},
+        {duration: "10s", target: 0},
+    ],
+    thresholds: {
+        http_req_duration: ["avg<200", "p(95)<500", "p(99)<1000"],
+    },
+};
+export default function () {
+    const cartId = createCart();
+    sleep(2);
+
+    addItemToCart(cartId, randomCartItem());
+    sleep(1);
+    addItemToCart(cartId, randomCartItem());
+    sleep(1);
+    addItemToCart(cartId, randomCartItem());
+
+    sleep(2);
+    getCart(cartId)
+}
+
+function createCart() {
+    let createCartResponse = http.post(`${baseUrl}/carts`, params);
+    check(createCartResponse, {
+        "createCartResponse is status 200": (r) => r.status === 200,
+    });
+
+    return JSON.parse(createCartResponse.body).id;
+}
+
+function addItemToCart(cartId, item) {
+    let addItemToCartResponse = http.post(`${baseUrl}/carts/${cartId}/items`, JSON.stringify(item), params);
+    check(addItemToCartResponse, {
+        "addItemToCartResponse is status 200": (r) => r.status === 200,
+    });
+}
+
+function getCart(cartId) {
+    let getCartResponse = http.get(`${baseUrl}/carts/${cartId}`, params);
+    check(getCartResponse, {
+        "getCart is status 200": (r) => r.status === 200,
+    });
+}
+
+// eslint-disable-next-line max-len
+const products = [
+    "Bananas", "Apples", "Tomatos", "Potatoes", "Carrots", "Cucumbers", "Peppers", "Onions", "Garlic", "Ginger"
+];
+
+function randomCartItem() {
+    return {
+        productName: products[Math.floor(Math.random() * products.length)],
+        quantity: Math.floor(Math.random() * 10) + 1,
+        itemPriceInEuroCents: Math.floor(Math.random() * 10) * 100 + 95,
+    };
+}
